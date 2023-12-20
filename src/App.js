c277b309b9831f7fee7b0dde85e38d0fb834b7a3
Eesa Wazir <eesawazir9@gmail.com>
2023-12-20 14:12:35 +0800

    Code for submit new post to feed added and file for post created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './styles/app.css';
 import Header from './components/Header';
 import SideBar from './components/SideBar';
+import Feed from './components/Feed';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       {/* App Body */}
       <div className="app__body">
         <SideBar />
-        {/* Feed */}
+        <Feed />
         {/* Widgets */}
       </div>
 
---
+++ src/components/Post.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/post.css'
+
+const Post = () => {
+    return (
+        <div>Post</div>
+    )
+}
+
+export default Post
